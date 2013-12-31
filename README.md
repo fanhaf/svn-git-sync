54c92b1e78bb49aa30ccd9434ac27dd435ee2afb
Michał Guminiak <michal.guminiak@tomtom.com>
2013-12-31 10:26:46 +0100

    Adding raw scripts.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ svn-git-sync
 ============
 
 A set of scripts and hooks to sync SVN and git repositories.
+
+It allowed one quite big team to gradually migrate from SVN to git. :)
+
+Do not run the scripts just yet - I need to clean it up...
+
---
+++ workflow-test.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+set -x
+
+rm -rf ~/src/gitfacade
+sudo rm -rf /home/git/fetchers/*
+killall svnserve
+
+ROOT=~/src/gitfacade
+FROOT=/home/git/fetchers
+mkdir -p $ROOT ; cd $ROOT
+
+git clone git@localhost:gitolite-admin
+cd gitolite-admin
+
+sed -i 7,8d conf/gitolite.conf
+git commit -am"removing coredb"
+git push origin master
+
+sudo rm /home/git/repositories/coredb.git -r
+
+echo "repo  coredb" >> conf/gitolite.conf
+echo "      RW+ =   @all" >> conf/gitolite.conf
+
+git commit -am"adding coredb"
+git push origin master
+
+sudo ln /home/git/pre-receive /home/git/repositories/coredb.git/hooks/pre-receive -s
+sudo ln /home/git/post-receive /home/git/repositories/coredb.git/hooks/post-receive -s
+
+cd ..
+
+svnadmin create svn
+
+cd svn
+cat >| conf/svnserve.conf <<SVN
+[general]
+anon-access=none
+auth-access=write
+realm=My first repo
+password-db=passwd
+SVN
+
+cat >| conf/passwd <<SVN
+[users]
+harry=harryssecret
+sally=sallyssecret
+guminiak=secret
+SVN
+
+cat >| conf/authz <<SVN
+[repository:/experiments]
+harry=rw
+sally=rw
+guminiak=rw
+SVN
+
+cd ..
+
+svn mkdir --parents file://$ROOT/svn/experiments/{trunk,branches,tags} -m 'Initial structure'
+svnserve -d -r $ROOT/svn
+
+echo "Created server"
+
+svn co --username harry --password harryssecret svn://localhost/experiments/ svnwork 
+
+cd svnwork/trunk
+echo "Mashed Potatoes" >> recipe
+svn add recipe
+svn commit --username harry --password harryssecret -m 'Added recipe.'
+
+cd $FROOT 
+git svn clone --username=guminiak -s svn://localhost/experiments/ svn2git-coredb
+cd svn2git-coredb
+
+git remote add gitlab git@localhost:coredb
+sudo chown -R git:git $FROOT/*
+
+git push gitlab master
+
+cd $ROOT
+git clone git@localhost:coredb work
+cd work
+git push origin :testbranch :testbranch2
+
+echo '**********************************'
+echo 'Simple push single commit scenario'
+echo '**********************************'
+cd $ROOT/work
+cat >> recipe <<ing
+4 Potatoes, diced
+salt, 4 cups
+water, 1 tablespoon
+ing
+git add recipe
+git ci -m 'Added ingredients list.'
+git push 2>&1 | tee $ROOT/tmp
+fgrep "Committing to svn://localhost/experiments/trunk" $ROOT/tmp || exit 1
+fgrep "Some branches could not be pushed to SVN" $ROOT/tmp && exit 1
+
+echo Single push success;
+
+echo '**********************************'
+echo 'Simple push next commit scenario'
+echo '**********************************'
+cd $ROOT/work
+git pull --rebase
+cat >> recipe <<ing
+Servings: 3
+ing
+git ci -am 'Added Serving size.'
+git push 2>&1 | tee $ROOT/tmp
+fgrep "Committing to svn://localhost/experiments/trunk" $ROOT/tmp || exit 1
+fgrep "Some branches could not be pushed to SVN" $ROOT/tmp && exit 1
+
+
+echo '**********************************'
+echo 'Changing from svn scenario'
+echo '**********************************'
+cd $ROOT/svnwork/trunk
+svn update
+cat >| shopping <<LIST
+Potatoes
+LIST
+svn add shopping
+svn commit -m 'Started a shopping list.'
+
+sudo su -l git -c $FROOT/../bin/sync-coredb
+
+cd $ROOT/work
+cat >> recipe <<INST
+Begin by adding salt to the water and bringing it to a boil.
+INST
+
+git ci -am "Added step 1"
+git push
+PUSH_RES=$?
+if [ $PUSH_RES = 0 ]; then
+    exit 1
+fi
+
+git push 2>&1 | tee $ROOT/tmp
+fgrep "master -> master (non-fast-forward)" $ROOT/tmp || exit 1
+
+git pull --rebase
+git push 2>&1 | tee $ROOT/tmp
+fgrep "Committing to svn://localhost/experiments/trunk" $ROOT/tmp || exit 1
+fgrep "Some branches could not be pushed to SVN" $ROOT/tmp && exit 1
+
+echo '****************************************'
+echo 'Updating from git with SVN changes newer'
+echo '****************************************'
+cd $ROOT/svnwork/trunk
+svn update
+sed -i 's/salt, 4 cups/Salt, 4 tablespoons/' recipe
+svn commit -m 'Reduced the salt level.'
+
+cd $ROOT/work
+git pull --rebase
+sed -i 's/salt, 4 cups/Salt, 1 tablespoon/' recipe
+sed -i 's/water, 1 tablespoon/Water, 1 cup/' recipe
+git ci -am 'Fixed salt/water qty mismatch'
+
+git push 2>&1 | tee push.log
+grep -q "Some branches could not be pushed to SVN." push.log
+PUSH_RESULT=$?
+if [ "$PUSH_RESULT" = "0" ]; then
+    exit 1
+fi
+
+#git push 2>&1 | tee $ROOT/tmp
+#fgrep "Some branches could not be pushed to SVN." $ROOT/tmp || exit 1
+
+
+echo '****************************************'
+echo 'Pushing different branch than controlled'
+echo '****************************************'
+git push origin master:testbranch 2>&1 | tee $ROOT/tmp
+fgrep "master -> testbranch" $ROOT/tmp || exit 1
+
+
+echo '****************************************'
+echo 'Pushing multiple branches'
+echo '****************************************'
+
+cd $ROOT/work
+git pull --rebase
+sed -i 's/tablespoon/small spoon/' recipe
+git ci -am 'Changed tablespoon to small spoon'
+
+git push origin master:master master:testbranch2 2>&1 | tee $ROOT/tmp
+fgrep "Committing to svn://localhost/experiments/trunk" $ROOT/tmp || exit 1
+fgrep "Some branches could not be pushed to SVN" $ROOT/tmp && exit 1
+fgrep "master -> testbranch" $ROOT/tmp || exit 1
+
+echo '****************************************'
+echo 'Pushing while update in progress'
+echo '****************************************'
+
+cd $ROOT/work
+git pull --rebase
+sed -i 's/small spoons/tablespoons/' recipe
+git ci -am 'Fixing one instance of small spoons'
+
+(
+    flock 9
+    git push origin master:master 2>&1 | tee $ROOT/tmp
+    fgrep "SVN update is in progress, please push again in a moment!" $ROOT/tmp || exit 1
+) 9</tmp/gitcoredbupdate.lockfile 
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+git push origin master:master 2>&1 | tee $ROOT/tmp
+fgrep "Committing to svn://localhost/experiments/trunk" $ROOT/tmp || exit 1
+fgrep "Some branches could not be pushed to SVN" $ROOT/tmp && exit 1
+
+echo 'SUCCESS!'
